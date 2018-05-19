72db52770c030e336793d1a907c5df9319afb02d
nlsnjohnson <nlsnjohnson@gmail.com>
2018-05-19 11:01:05 +0200

    finished masses

--- masses.py
+++ masses.py
@@ -28,6 +28,9 @@ R_t = D/2.0
 W_g_kg = 19051  # [kg]
 W_g = f_lb*W_g_kg
 
+#  Empty Weight
+W_oe_kg = 12020
+W_oe = W_oe_kg*f_lb
 
 #  Tandem Constant for Tail Structure TODO figure this out further?
 K_t = 0
@@ -42,10 +45,12 @@ S_tt = 8.41 * f_ft**2
 
 #  Total Body Surface Area approximated with Square prism.
 #  Fuselage Width and height [ft]
-w_f_m = 2.68
+w_f_m = 2.68  # [m]
+w_f_m = 2.5
 w_f = w_f_m*f_ft
 #  Fusselage Length
-l_f_m = 19.07
+l_f_m = 19.07 # [m]
+l_f_m = 12.54
 l_f = l_f_m*f_ft
 S_b = w_f*w_f*l_f
 
@@ -82,7 +87,8 @@ S_pl = n*c*R
 
 #  2: Total Rotor weight
 W_2 = -194.685+12.164*S_pl
-WF_2 = W_2/W_g*100
+WF_2 = W_2/W_oe*100
+
 
 #  3: Tail Weight (Tail Rotor and Structure)
 #  Tail Rotor Weight
@@ -91,47 +97,59 @@ W_3A = W_g**1.352 / exp(8.327)
 #  Tail Structure Weight
 W_3B = -17.872+2.829*S_tt + K_t
 W_3 = W_3A + W_3B
-WF_3 = W_3/W_g*100
+WF_3 = W_3/W_oe*100
+
 
 #  4: Body Weight
 W_4 = -269.023 + 2.356*S_b
-WF_4 = W_4/W_g*100
+WF_4 = W_4/W_oe*100
+
 
 #  5: Landing Gear Weight
 W_5 = -5.489 + 0.0342*W_g
-WF_5 = W_5/W_g*100
+WF_5 = W_5/W_oe*100
+
 
 #  6: Nacelle Weight
 W_6 = -64.779 + 2.401*S_n
-WF_6 = W_6/W_g*100
+WF_6 = W_6/W_oe*100
+
 
 #  7: Propulsion System Weights engine = 2x GE T64-GE-413
-#  Power Plant Weight
-#W_7A = 408.198 + 0.192*HP_e
-W_7A = 327*f_lb*2
+#  Power Plant Weight 327 lb each found online
+W_7A = 408.198 + 0.192*HP_e
+#W_7A = 755.0*2.0
+
 #  Drive System Weight
 W_7B = -35.551+0.101*W_g
+
 #  Fuel Tank Weights
 W_7C = 10.974 + 0.790*G
+
 #  Complete Power Plant weight
 W_7 = W_7C+W_7B+W_7A
-WF_7 = W_7/W_g*100
+WF_7 = W_7/W_oe*100
+
 
 #  8: Flight Controls Weight [lb]
 W_8 = 62.025 + 0.0334*W_g
-WF_8 = W_8/W_g*100
+WF_8 = W_8/W_oe*100
+
 
 #  9: Auxiliary Power Weight [lb]
 W_9 = 157
-WF_9 = W_9/W_g*100
+WF_9 = W_9/W_oe*100
+
 
 #  10: Instruments Weight [lb]
 W_10 = 50.507 + 0.0267*HP_e
-WF_10 = W_10/W_g*100
+WF_10 = W_10/W_oe*100
+
 
 #  11: Hydraulics Weight [lb]
 W_11 = 15.89+0.00446*W_g
-WF_11 = W_11/W_g*100
+WF_11 = W_11/W_oe*100
+
 
 #  12: Pneumatics Neglected
 
@@ -139,53 +157,67 @@ WF_11 = W_11/W_g*100
 #  13: Electrical System Mass [lb]
 #  W_13 = 139.947 + 0.234*S_b
 W_13 = exp(0.903)*S_b**.733
-WF_13 = W_13/W_g*100
+WF_13 = W_13/W_oe*100
 
 #  14: Avionics Weight [lb]
 W_14 = -59.041 + 0.0175*W_g + 0.348*r
-WF_14 = W_14/W_g*100
+WF_14 = W_14/W_oe*100
 
 #  15: Furnishings and Equipment [lb]
 W_15 = -8.106 + 0.176*S_b + 20.456*(N_p + N_c)
-WF_15 = W_15/W_g*100
+WF_15 = W_15/W_oe*100
 
 #  16 & 17: Air Conditioning and Anti-Icing
 W_1617 = 28.844 + 0.0730*S_b
-WF_1617 = W_1617/W_g*100
+WF_1617 = W_1617/W_oe*100
 
 #  18: Load and Handling [lb]
 W_18 = -71.875 + 0.111*S_b + 3.489*(N_p + N_c)
-WF_18 = W_18/W_g*100
+WF_18 = W_18/W_oe*100
 
 #  Payload [lb]
 W_pl = 14000
-WF_pl = W_pl/W_g*100
+WF_pl = W_pl/W_oe*100
 
-#  Sum of weight fractions percent of MTOW
-W_tot = WF_2+WF_3+WF_4+WF_5+WF_6+WF_7+WF_8+WF_9+WF_10+WF_11+WF_13+WF_14+WF_15+WF_1617+WF_18+WF_pl
+#  Fuel Weight
+V_f_m = 2415.1  # [l]
+rho_f_m = .7750 # [kg/l]
+W_f_kg = rho_f_m*V_f_m
+W_f = W_f_kg*f_lb
+WF_f = W_f/W_oe
+
+
+#  Sum of weight fractions percent of M_oe
+WF_oe = (WF_2+WF_3+WF_4+WF_5+WF_6+WF_7+WF_8+WF_9+WF_10+WF_11+WF_13+WF_14+WF_15+WF_1617+WF_18)/100
+
+#  Sum of weight fractions of W_g
+WF_tot = (W_2+W_3+W_4+W_5+W_6+W_7+W_8+W_9+W_10+W_11+W_13+W_14+W_15+W_1617+W_18+W_pl+W_f)*100/W_g
 
 print 'Gross Weight                 = ', W_g, 'lbs'
-print '2:  Total Main Rotor Weight  = ', W_2, 'lbs. Percent of W_g = ', WF_2
-#  print '3A: Tail Rotor Weight        = ', W_3A, 'lbs. Percent of W_g = ', W_3A/W_g*100
-#  print '3B: Tail Structure Weight    = ', W_3B, 'lbs. Percent of W_g = ', W_3B/W_g*100
-print '3: Tail Weight               = ', W_3, 'lbs. Percent of W_g = ', WF_3
-print '4:  Body Weight              = ', W_4, 'lbs. Percent of W_g = ', WF_4
-print '5:  Landing Gear Weight      = ', W_5, 'lbs. Percent of W_g = ', WF_5
-print '6:  Nacelle Weight           = ', W_6, 'lbs. Percent of W_g = ', WF_6
-#  print '7A: Power Plant Weight       = ', W_7A, 'lbs. Percent of W_g = ', W_7A/W_g*100
-#  print '7B: Drive System Weight      = ', W_7B, 'lbs. Percent of W_g = ', W_7B/W_g*100
-#  print '7C: (Main) Fuel Tank Weight  = ', W_7C, 'lbs. Percent of W_g = ', W_7C/W_g*100
-print '7:  Propulsion System Weight = ', W_7, 'lbs. Percent of W_g = ', WF_7
-print '8:  Flight Controls Weight   = ', W_8, 'lbs. Percent of W_g = ', WF_8
-print '9:  Auxiliary Power Weight   = ', W_9, 'lbs. Percent of W_g = ', WF_9
-print '10: Instruments Weight       = ', W_10, 'lbs. Percent of W_g = ', WF_10
-print '11: Hydraulics Weight        = ', W_11, 'lbs. Percent of W_g = ', WF_11
+print '2:  Total Main Rotor Weight  = ', W_2, 'lbs. Percent of W_oe = ', WF_2
+#  print '3A: Tail Rotor Weight        = ', W_3A, 'lbs. Percent of W_oe = ', W_3A/W_g*100
+#  print '3B: Tail Structure Weight    = ', W_3B, 'lbs. Percent of W_oe = ', W_3B/W_g*100
+print '3: Tail Weight               = ', W_3, 'lbs. Percent of W_oe = ', WF_3
+print '4:  Body Weight              = ', W_4, 'lbs. Percent of W_oe = ', WF_4
+print '5:  Landing Gear Weight      = ', W_5, 'lbs. Percent of W_oe = ', WF_5
+print '6:  Nacelle Weight           = ', W_6, 'lbs. Percent of W_oe = ', WF_6
+print '7A: Power Plant Weight       = ', W_7A, 'lbs. Percent of W_oe = ', W_7A/W_g*100
+print '7B: Drive System Weight      = ', W_7B, 'lbs. Percent of W_oe = ', W_7B/W_g*100
+print '7C: (Main) Fuel Tank Weight  = ', W_7C, 'lbs. Percent of W_oe = ', W_7C/W_g*100
+print '7:  Propulsion System Weight = ', W_7, 'lbs. Percent of W_oe = ', WF_7
+print '8:  Flight Controls Weight   = ', W_8, 'lbs. Percent of W_oe = ', WF_8
+print '9:  Auxiliary Power Weight   = ', W_9, 'lbs. Percent of W_oe = ', WF_9
+print '10: Instruments Weight       = ', W_10, 'lbs. Percent of W_oe = ', WF_10
+print '11: Hydraulics Weight        = ', W_11, 'lbs. Percent of W_oe = ', WF_11
 print '12: Pneumatics Weight is Neglected'
-print '13: Electronics Weight       = ', W_13, 'lbs. Percent of W_g = ', WF_13
-print '14: Avionics Weight          = ', W_14, 'lbs. Percent of W_g = ', WF_14
-print '15: Furnishings and Equipment Weight = ', W_15, 'lbs. Percent of W_g = ', WF_15
-print '16& 17: Air Conditioning and Anti-Icing Weight = ', W_1617, 'lbs. Percent of W_g = ', WF_1617
-print '18: Loads and Handling Weight = ', W_18, 'lbs. Percent of W_g = ', WF_18
-print 'xx: Payload Weight            = ', W_pl, 'lbs. Percent of W_g = ', WF_pl
-print 'Sum of weight fractions', W_tot
+print '13: Electronics Weight       = ', W_13, 'lbs. Percent of W_oe = ', WF_13
+print '14: Avionics Weight          = ', W_14, 'lbs. Percent of W_oe = ', WF_14
+print '15: Furnishings and Equipment Weight = ', W_15, 'lbs. Percent of W_oe = ', WF_15
+print '16& 17: Air Conditioning and Anti-Icing Weight = ', W_1617, 'lbs. Percent of W_oe = ', WF_1617
+print '18: Loads and Handling Weight = ', W_18, 'lbs. Percent of W_oe = ', WF_18
+print 'xx: Payload Weight            = ', W_pl, 'lbs. Percent of W_oe = ', WF_pl
+print 'xx: Fuel Weight               = ', W_f, 'lbs. Percent of W_oe = ', WF_f
+print 'Sum of Component OE weight fraction', WF_oe
+print 'Sum of Component Gross weight fraction', WF_tot
+
 
