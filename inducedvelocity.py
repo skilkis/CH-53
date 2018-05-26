d336904a76cbce20c8edef0797b5d758ef83bbc5
skilkis <sankilkis@msn.com>
2018-05-26 22:34:26 +0200

    Problem 4-3 for the Haffner Diagram is Done

--- inducedvelocity.py
+++ inducedvelocity.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
 import os
 from math import sin, cos, asin, degrees
+from collections import Iterable
 
 N = 100  # Fidelity of the arrays (Higher is better)
 _working_dir = os.getcwd()  # Added for save functionality
@@ -27,6 +28,10 @@ v_i_lowspeed = []
 for i in range(0, len(V_bar)):
 
     def func(x):
+        """ Defines a 4-th order equation v_i**4 + (V**2 * v_i**2) - 1 = 0
+
+        :param x: Represents the Non-Dimensional Induced Velocity
+        """
         return x**4 + ((V_bar[i]) ** 2 * (x ** 2)) - 1
 
     v_i_lowspeed.append(fsolve(func, x0=np.array([1])))
@@ -43,17 +48,43 @@ while error > 0.001:
 
 idx = i
 
+
 # Utilizing Numerical Methods to Solve w/o Assumptions:
-D_par = sum_cds * rho * V**2
-alpha_rad = [asin(D/W) for D in D_par]
+def alpha_disk(velocity):
+    """ A function that returns the rotor disk angle of attack (AoA) per input flight velocity
+
+    :param velocity: Forward flight velocity (gamma=0) in SI meter per second [m/s]
+    :return: Rotor Disk Angle of Attack (AoA) in SI radians [rad]
+    :rtype: list or float
+    """
+    D_par = sum_cds * rho * velocity ** 2  # Parasitic Drag Value
+    if isinstance(velocity, Iterable) and type(velocity) is not str:
+        alpha = [asin(D/W) for D in D_par]
+    else:
+        alpha = asin(D_par/W)
+    return alpha
+
+
+alpha_rad = alpha_disk(velocity=V)
 alpha_deg = [degrees(rad) for rad in alpha_rad]
+
+V_bar_cr = V_cr / v_i_hover
+alpha_rad_cr = alpha_disk(velocity=V_cr)
+alpha_deg_cr = degrees(alpha_rad_cr)
 
 fig = plt.figure('AlphavsVelocity')
 plt.style.use('ggplot')
-plt.plot(V, alpha_deg)
+plt.plot(V_bar, alpha_deg, label=r'Function')
 plt.title(r'Disk Angle of Attack as a Function of Velocity')
-plt.xlabel(r'Velocity [m/s]')
+plt.plot(V_bar_cr, alpha_deg_cr,
+         marker='o',
+         markerfacecolor='white',
+         markeredgecolor='black', markeredgewidth=1,
+         linewidth=0,
+         label=r'Cruise Condition ($\overline{V}=$ %0.1f [-], $\alpha_d$ = %0.1f [deg])' % (V_bar_cr, alpha_deg_cr))
+plt.xlabel(r'Non-Dimensional Velocity $\overline{V}=\frac{\mu}{\sqrt{C_T/2}}$')
 plt.ylabel(r'Disk Angle of Attack [deg]')
+plt.legend(loc='best')
 plt.show()
 fig.savefig(fname=os.path.join(_working_dir, 'Figures', '%s.pdf' % fig.get_label()), format='pdf')
 
@@ -63,6 +94,10 @@ for i in range(0, len(V_bar)):
     alpha_rad = asin(D_par / W)
 
     def func(x):
+        """ Defines a 2-th order equation function (V*sin(a) + v_i)**2 + (V*cos(a))**2 - (1/v_i)**2 = 0
+
+        :param x: Represents the Non-Dimensional Induced Velocity
+        """
         return ((V_bar[i]*sin(alpha_rad)+abs(x))**2 + (V_bar[i]*cos(alpha_rad))**2) - (1/(abs(x)**2))
 
     v_i.append(abs(fsolve(func, x0=np.array([1]))[0]))
@@ -82,8 +117,13 @@ plt.plot(V_bar[:idx+1], v_i_lowspeed[:idx+1], label=r'Low-Speed')
 plt.plot(V_bar[idx:], v_i_highspeed[idx:], label='High-Speed')
 
 # No Assumptions:
-plt.plot(V_bar, v_i, label='Numerical Solution', marker='o')
+# plt.plot(V_bar, v_i, label='Numerical Solution', marker='o')
 
+# Obtaining Values for Haffner Diagram Check at Cruise Speed (Question 4-3)
+haffner_x = V_bar_cr * cos(alpha_rad_cr)
+haffner_y = V_bar_cr * sin(alpha_rad_cr)
+
+print 'Haffner Diagram x,y = (%0.1f, %0.1f) for V_cr = %0.1f' % (haffner_x, haffner_y, V_bar_cr)
 
 plt.title('Induced Velocity as a Function of Forward Velocity')
 plt.xlabel(r'Non-Dimensional Velocity $\overline{V}=\frac{\mu}{\sqrt{C_T/2}}$')
