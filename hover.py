3c4e44c5cbd60c50c78bf86896ec01919c114c13
skilkis <sankilkis@msn.com>
2018-06-07 20:49:02 +0200

    AoA of the Blade has been verified

--- hover.py
+++ hover.py
@@ -13,8 +13,12 @@ import numpy as np
 from scipy import integrate
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 from math import radians, sqrt, pi, degrees, cos, sin
 from basic_units import radians as rad_ticks  # Renaming to remove conflict with built-in package
+import os  # Necessary to determining the current working directory to save figures
+
+_working_dir = os.getcwd()
 
 
 class Attribute(object):
@@ -34,6 +38,9 @@ class Attribute(object):
 
 
 class HoverFlapping(Constants):
+    """ Defines the Flapping Dynamics of the CH-53D in Hovering Flight.
+
+    :param collective_pitch: Collective Pitch of the Main Rotor Blades in SI radian [rad]"""
 
     def __init__(self, collective_pitch=radians(8)):
         self.collective_pitch = collective_pitch
@@ -101,11 +108,11 @@ class HoverFlapping(Constants):
 
     def ode(self, x, t):
         """ Defines the blade-flapping velocity and acceleration as a function of the blade-flapping deflection and
-        velocity in state-space form.
+        velocity in a system of 1st Order ODEs.
 
         :param x: The state-space vector that is equal to the [\beta  \dot{\beta}]'
         :param t: Dimensional time in SI seconds [s]
-        :return: State-Space Model for the Blade Flapping Angle \beta
+        :return: System of 1st Order ODEs for the Blade Flapping Angle \beta
         :rtype: list
         """
 
@@ -150,12 +157,18 @@ class HoverFlapping(Constants):
         return interp1d(rad_interval, sol, kind='slinear')
 
     def alpha_psi(self, azimuth, radius):
+        """ Conputes the Angle of Attack (AoA) of an advancing blade element with the assumption of constant inflow
+
+        :param azimuth: The current azimuth angle of the advancing blade element in SI radian [rad]
+        :param radius: The current radius of the advancing blade element in SI meter [m]
+        :return: AoA of the advancing blade element in SI [rad]
+        """
         return self.collective_pitch - ((self.hover_induced_velocity + (self.flap_velocity_psi.__call__(azimuth) *
                                                                         radius)) / (self.main_rotor.omega * radius))
 
     def plot_alpha(self):
         azimuth = np.linspace(0, 2*pi, 360)
-        radii = np.linspace(0.1, self.main_rotor.radius, 60)
+        radii = np.linspace(3, self.main_rotor.radius, 60)
 
         fig = plt.figure('BladeAoA')
         plt.style.use('ggplot')
@@ -171,44 +184,79 @@ class HoverFlapping(Constants):
         cmap = plt.get_cmap('jet')
         cmap_grey = plt.get_cmap('Greys')
         X, Y = np.meshgrid(azimuth, radii)
-        scatter_plot = ax.scatter(X, Y, c=alpha, cmap=cmap)
-        contour_plot = ax.contour(X, Y, alpha, cmap=cmap_grey)
+        # scatter_plot = ax.scatter(X, Y, c=alpha, cmap=cmap)
+        contour_plot = ax.contour(X, Y, alpha, cmap=cmap, vmin=-6, vmax=5)
         ax.set_rlabel_position(-22.5)  # get radial labels away from plotted line
         ax.set_theta_zero_location("S")
         ax.set_rlim(0, self.main_rotor.radius)
         ax.grid(True)
-        plt.title('')
         plt.clabel(contour_plot, inline=1, fontsize=10)
-        plt.colorbar(scatter_plot, shrink=0.8, extend='both')
+        # plt.colorbar(scatter_plot, shrink=0.8, extend='both')
 
-        ax.set_title("Angle of Attack Variation During Hover due to Flapping", va='bottom')
+        ax.set_title("Adv. Blade Element AoA vs. Azimuth and Radius", va='bottom')
+        ax.set_xlabel(r'Blade Azimuth $\psi$ [rad]')
         plt.show()
+        fig.savefig(fname=os.path.join(_working_dir, 'Figures', '%s.pdf' % fig.get_label()), format='pdf')
+        return 'Plot Created and Saved'
 
-    def plot_flapangle(self):
-        time_interval = np.linspace(0, 1, 1000)
-        fig = plt.figure('PsivsBeta')
+    # def plot_flapangle(self):
+    #     time_interval = np.linspace(0, 1, 1000)
+    #     fig = plt.figure('psivsBeta')
+    #     plt.style.use('ggplot')
+    #     ax = fig.gca()
+    #     sol = self.ode_solver(time_interval, ic=self.initial_condition)[0]
+    #     blade_position = [(t * self.main_rotor.omega) * rad_ticks for t in time_interval]
+    #     ax.plot(blade_position, sol, xunits=rad_ticks)
+    #     plt.title(r'Blade Angular Displacement Response $\beta_0, \dot{\beta}_0 = \left(0, 0\right)$')
+    #     plt.xlabel(r'Blade Azimuth $\psi$ [rad]')
+    #     plt.ylabel(r'Blade Flapping Angle  $\beta_0 = 0$ [rad]')
+    #     plt.show()
+    #     fig.savefig(fname=os.path.join(_working_dir, 'Figures', '%s.pdf' % fig.get_label()), format='pdf')
+    #     return 'Plot Created and Saved'
+    #
+    # def plot_bladevelocity(self):
+    #     time_interval = np.linspace(0, 1, 1000)
+    #     fig = plt.figure('psivsBeta')
+    #     plt.style.use('ggplot')
+    #     ax = fig.gca()
+    #     sol = self.ode_solver(time_interval, ic=self.initial_condition)[1]
+    #     blade_position = [(t * self.main_rotor.omega) * rad_ticks for t in time_interval]
+    #     ax.plot(blade_position, sol, xunits=rad_ticks)
+    #     plt.title(r'Blade Angular Velocity Response $\beta_0, \dot{\beta}_0 = \left(0, 0\right)$')
+    #     plt.xlabel(r'Blade Azimuth $\psi$ [rad]')
+    #     plt.ylabel(r'Blade Flapping Velocity  $\beta_0 = 0$ [rad/s]')
+    #     plt.show()
+    #     fig.savefig(fname=os.path.join(_working_dir, 'Figures', '%s.pdf' % fig.get_label()), format='pdf')
+    #     return 'Plot Created and Saved'
+
+    def plot_response(self):
+        fig = plt.figure('BetaResponse_IC(%1.2f,%1.2f)' % (self.initial_condition[0], self.initial_condition[1]))
         plt.style.use('ggplot')
-        ax = fig.gca()
-        sol = self.ode_solver(time_interval, ic=self.initial_condition)[0]
-        blade_position = [(t * self.main_rotor.omega) * rad_ticks for t in time_interval]
-        ax.plot(blade_position, sol, xunits=rad_ticks)
-        plt.title(r'Blade Angular Displacement Response $\beta_0, \dot{\beta}_0 = \left(0, 0\right)$')
-        plt.xlabel(r'Blade Azimuth $\Psi$ [rad]')
-        plt.ylabel(r'Blade Flapping Angle  $\beta_0 = 0$ [rad]')
-        plt.show()
+        gs = gridspec.GridSpec(2, 1, top=0.9)
 
-    def plot_bladevelocity(self):
-        time_interval = np.linspace(0, 1, 1000)
-        fig = plt.figure('PsivsBeta')
-        plt.style.use('ggplot')
-        ax = fig.gca()
-        sol = self.ode_solver(time_interval, ic=self.initial_condition)[1]
+        # Call to ODE Solver for Plot Solution
+        time_interval = np.linspace(0, self.t_final * 2, 1000)
+        sol = self.ode_solver(time_interval, ic=self.initial_condition)
         blade_position = [(t * self.main_rotor.omega) * rad_ticks for t in time_interval]
-        ax.plot(blade_position, sol, xunits=rad_ticks)
-        plt.title(r'Blade Angular Velocity Response $\beta_0, \dot{\beta}_0 = \left(0, 0\right)$')
-        plt.xlabel(r'Blade Azimuth $\Psi$ [rad]')
-        plt.ylabel(r'Blade Flapping Velocity  $\beta_0 = 0$ [rad/s]')
+
+        # Displacement Plot
+        top_plot = fig.add_subplot(gs[0, 0])
+        top_plot.plot(blade_position, sol[0], xunits=rad_ticks)
+        top_plot.set_ylabel(r'$\beta$ [rad]')
+        top_plot.set_xlabel('')
+
+        # Velocity Plot
+        bot_plot = fig.add_subplot(gs[1, 0])
+        bot_plot.plot(blade_position, sol[1], xunits=rad_ticks)
+        bot_plot.set_xlabel(r'Blade Azimuth $\psi$ [rad]')
+        bot_plot.set_ylabel(r'$\dot{\beta}$ [rad\s]')
+
+        # Figure Title
+        plt.suptitle(r'Blade Angular Response $\beta_0, \dot{\beta}_0 = \left(%1.2f, %1.2f\right)$'
+                     % (self.initial_condition[0], self.initial_condition[1]))
+
         plt.show()
+        fig.savefig(fname=os.path.join(_working_dir, 'Figures', '%s.pdf' % fig.get_label()), format='pdf')
 
 
 if __name__ == '__main__':
@@ -216,6 +264,7 @@ if __name__ == '__main__':
 
     # obj.plot_flapangle()
     obj.plot_alpha()
+    obj.plot_response()
     # print obj.inflow_ratio
     # print obj.coning_angle
     # print obj.lock_number
